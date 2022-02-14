ed54fd3ae6e8b5e3dabb94fed04b6435d26c1fb1
Rustam <Rustamaaaa@bk.ru>
2022-02-14 19:09:20 +0300

    feat: Реализовать функцию сервис log.service.js

---
+++ services/log.service.js
@@ -0,0 +1,26 @@
+import chalk from "chalk";
+import dedent from 'dedent-js';
+const { bgRed, bgGreen, bgCyan, yellowBright } = chalk;
+
+const printError = (err) => {
+	const str = `${bgRed(" Error: ")} ${err}`;
+	console.log(str);
+};
+
+const printSuccess = (mes) => {
+  const str = `${bgGreen(" Success: ")} ${mes}`;
+  console.log(str);
+};
+
+const printHelp = () => {
+  const str = dedent`
+		\n${bgCyan(" Help: ")}
+		${yellowBright("Без параметров:")} вывод погоды;
+		${yellowBright("-s [city]:")} для установки городы;
+		${yellowBright("-h:")} для вывода помощи;
+		${yellowBright("-t [api_key]:")} для сохранения токена;
+	`;
+  console.log(str);
+};
+
+export { printError, printSuccess, printHelp };
--- weather.js
+++ weather.js
@@ -1,8 +1,19 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
+import { printHelp } from './services/log.service.js';
 
 const initCli = () => {
 	const args = getArgs(process.argv);
-	console.log(args)
+	if (args.h) {
+		// Вывод help
+		printHelp();
+  }
+	if (args.s) {
+    // Сохранить город
+  }
+	if (args.t) {
+    // Сохранить токен
+  }
+	// Вывести погоду
 };
 initCli();
