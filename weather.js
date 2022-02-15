3313f7c98af722aab2b805230f07844c46794c00
Rustam <Rustamaaaa@bk.ru>
2022-02-15 16:35:53 +0300

    feat: реализовать api.service.js с помощью https

--- weather.js
+++ weather.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { getWeather } from './services/api.service.js';
 import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
 
 const saveToken = async (token) => {
+	if (!token.length) {
+		printError('Не передан токен');
+		return;
+	}
 	try {
-		await saveKeyValue("token", token);
+		await saveKeyValue(TOKEN_DICTIONARY.token, token);
     printSuccess('Токен успешно сохранен');
 	} catch(e) {
 		printError(e.message);
@@ -26,5 +31,6 @@ const initCli = () => {
 		return saveToken(args.t);
   }
 	// Вывести погоду
+	getWeather("moscow");
 };
 initCli();
